3bf84135bcc6a11bb68fe0ad96dd7ad85f3ae992
asgmodel <145135565+asgmodel@users.noreply.github.com>
2025-03-14 00:26:02 +0300

    Update data.py

--- apps/chatbot/data.py
+++ apps/chatbot/data.py
@@ -1,1 +1,37 @@
+LANGUAGES = {
+    "ar": {
+        "options": "الخيارات",
+        "category": "التصنيف",
+         "model_name":"الموديل",
+        "choose_model":"اختر موديل",
+        "choose_category": "اختر تصنيفًا",
+        "language": "اللغة",
+        "choose_language": "اختر لغة",
+        "dialect": "اللهجة",
+        "choose_dialect": "اختر لهجة",
+        "settings": "الإعدادات",
+        "temperature": "درجة الحرارة",
+        "max_token": "أقصى عدد من الرموز",
+        "streaming": "البث",
+        "voice_output": "الإخراج الصوتي",
+        "enter_message": "أدخل الرسالة أو قم بتحميل ملف..."
+    },
+    "en": {
+        "options": "Options",
+        "category": "Category",
+         "model_name":"model name",
+        "choose_model":"Choose a Model",
+        "choose_category": "Choose a category",
+        "language": "Language",
+        "choose_language": "Choose a language",
+        "dialect": "Dialect",
+        "choose_dialect": "Choose a dialect",
+        "settings": "Settings",
+        "temperature": "Temperature",
+        "max_token": "Max Token",
+        "streaming": "Streaming",
+        "voice_output": "Voice Output",
+        "enter_message": "Enter message or upload file..."
+    }
+}
 
