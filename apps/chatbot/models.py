f350538ccaa83c9e9354011d94195346c375ff75
asgmodel <145135565+asgmodel@users.noreply.github.com>
2025-03-14 00:21:51 +0300

    Update models.py

--- apps/chatbot/models.py
+++ apps/chatbot/models.py
@@ -1,1 +1,23 @@
+from dataclasses import dataclass
+from typing import Optional, List, Set
 
+@dataclass(frozen=True)
+class FilterModelAI:
+    name: Optional[str] = None
+    category: Optional[str] = None
+    language: Optional[str] = None
+    isStandard: Optional[bool] = None
+    gender: Optional[str] = None
+    dialect: Optional[str] = None
+    Type: Optional[str] = None
+
+@dataclass(frozen=True)
+class ModelAiCreate:
+    name: str
+    AbsolutePath:str
+    category: Optional[str] = None
+    language: Optional[str] = None
+    isStandard: Optional[bool] = None
+    gender: Optional[str] = None
+    dialect: Optional[str] = None
+    Type: Optional[str] = None
