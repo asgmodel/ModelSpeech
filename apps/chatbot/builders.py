93f1d2877c1d365c2133cf1e14ed3278129f2e88
asgmodel <145135565+asgmodel@users.noreply.github.com>
2025-03-15 02:28:22 +0300

    Update builders.py

--- apps/chatbot/builders.py
+++ apps/chatbot/builders.py
@@ -1,7 +1,7 @@
-from clients import *
-from typing import List
-from models import *
-from seeds import  RequestDev
+from .clients import *
+from .typing import List
+from .models import *
+from .seeds import  RequestDev
 
 class BuilderStudioModelAiAPi:
     def __init__(self, url: str, token: str) -> None:
