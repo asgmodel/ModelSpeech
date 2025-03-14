20d8ece1090fd9bd58bcb3407d41877b8d69ef1d
asgmodel <145135565+asgmodel@users.noreply.github.com>
2025-03-14 05:48:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,24 +24,24 @@ async def redirect_to_site():
 
 
 
-# app = gr.mount_gradio_app(app, demo, path='/studio-t2speech')
-# app = gr.mount_gradio_app(app, t2speech.demo, path='/t2speech')
+app = gr.mount_gradio_app(app, demo, path='/studio-t2speech')
+app = gr.mount_gradio_app(app, t2speech.demo, path='/t2speech')
 
-# app = gr.mount_gradio_app(app, t2text.demo, path='/studio-t2text')
+app = gr.mount_gradio_app(app, t2text.demo, path='/studio-t2text')
 
-# app = gr.mount_gradio_app(app, dash.demo, path='/dash')
+app = gr.mount_gradio_app(app, dash.demo, path='/dash')
 
-# app = gr.mount_gradio_app(app, chatbot.demo, path='/chatbot')
-# app = gr.mount_gradio_app(app, dashboard.dashboard, path='/dashboard')
-# app = gr.mount_gradio_app(app, t2speechmuit.demo, path='/t2speechmuit')
-# app = gr.mount_gradio_app(app, userspace.app, path='/createspace')
+app = gr.mount_gradio_app(app, chatbot.demo, path='/chatbot')
+app = gr.mount_gradio_app(app, dashboard.dashboard, path='/dashboard')
+app = gr.mount_gradio_app(app, t2speechmuit.demo, path='/t2speechmuit')
+app = gr.mount_gradio_app(app, userspace.app, path='/createspace')
 
-# app = gr.mount_gradio_app(app, audio_interface.demo, path='/manger-audio')
+app = gr.mount_gradio_app(app, audio_interface.demo, path='/manger-audio')
 
 
 
-from apps.ui_apps import APPS
-for uiapp,path in APPS:
-    app = gr.mount_gradio_app(app, uiapp, path="/"+path)
+# from apps.ui_apps import APPS
+# for uiapp,path in APPS:
+#     app = gr.mount_gradio_app(app, uiapp, path="/"+path)
 
     
