46dc8db39040cfdea72523fdbac5cd8fdab77869
Anas Al-Tawil <145135565+asgmodel@users.noreply.github.com>
2025-05-20 15:20:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def run_model(input_data: ModelInput):
 
 app = gr.mount_gradio_app(app, t2speechmuit.demo, path='/t2speechmuit')
 app = gr.mount_gradio_app(app, testc.demo, path='/testc')
-app = gr.mount_gradio_app(app, chataist.demo, path='/chat-pro')
+# app = gr.mount_gradio_app(app, chataist.demo, path='/chat-pro')
 app = gr.mount_gradio_app(app, chatasg.demo, path='/asg')
 
 app = gr.mount_gradio_app(app, audio_interface.demo, path='/studio-pro')
